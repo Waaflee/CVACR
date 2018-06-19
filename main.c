4c3f61aa3c838ffc978b03329a4e29c8ba25a3c0
Waaflee <agustinwafle@gmail.com>
2018-06-19 16:58:56 -0300

    Created custom command interpreter

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include "./lib/AVRDuino/A4988.h"
 #include "./lib/AVRDuino/core.h"
-#include "./lib/AVRDuino/uart.h"
-#include "./lib/AVRDuino/pwm.h"
-#include "./lib/AVRDuino/timers.h"
 #include "./lib/AVRDuino/interrupts.h"
+#include "./lib/AVRDuino/timers.h"
+#include "./lib/AVRDuino/uart.h"
 #include "lib/custom/command_interpreter.h"
 #include <util/delay.h>
 
@@ -12,14 +11,26 @@ FILE uart_io = FDEV_SETUP_STREAM(uecho, uread, _FDEV_SETUP_RW);
 int main(void) {
   stdout = stdin = &uart_io;
   UART_init(checkData);
-  // DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 30};
-  // pololu STP1 = newPololuFA(STPArray1);
-  // STEPPER PAP1;
-  // PAP1.motor = &STP1;
-  // PAP1.enabled = 0;
-  // PAParray[0] = &PAP1;
+
+  DriveArray STPArray1 = {2, 3, 4, 0, 0, 0, 1.8, 10};
+  pololu STP1 = newPololuFA(STPArray1);
+  STEPPER PAP1;
+  PAP1.motor = &STP1;
+  PAP1.enabled = 0;
+  PAParray[0] = &PAP1;
+
+  // setPCInt(8);
+  // setPCInt(9);
+
+  // PAPsInit(8);
+  setTimer0(x8);
+
+  sei();
 
   setPin(13, OUTPUT);
+  _delay_ms(500);
+  printf("Setup Complete\n");
+
   while (1) {
     /* code */
     togglePin(13);
@@ -27,3 +38,12 @@ int main(void) {
   }
   return 0;
 }
+
+ISR(PCINT0_vect) {
+  _delay_ms(10);
+  if (readDPin(8)) {
+    raceEnd(0, START);
+  } else if (readDPin(9)) {
+    raceEnd(0, END);
+  };
+};
