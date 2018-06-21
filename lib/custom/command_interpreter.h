68126d1b9a256b4813d42b2520bd10940e1e87d4
Waaflee <agustinwafle@gmail.com>
2018-06-21 12:32:02 -0300

    added pwm based led, that brights proportionally to the position

--- lib/custom/command_interpreter.h
+++ lib/custom/command_interpreter.h
@@ -2,7 +2,12 @@
 #include "../AVRDuino/core.h"
 #include "../AVRDuino/uart.h"
 #include "../AVRDuino/A4988.h"
-#include "../AVRDuino/timers.h"
+#include "../AVRDuino/pwm.h"
+
+struct pwm1 pwm;
+
+enum estados {activado, desactivado, homing, referenciado};
+volatile extern enum estados estado;
 
 void help();
 void checkData(char data[]);
