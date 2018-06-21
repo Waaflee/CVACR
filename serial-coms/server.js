90951b2e4b0e8907e7f9f930f7111caae30449c2
Waaflee <agustinwafle@gmail.com>
2018-06-21 02:46:24 -0300

    Improving express js app

--- serial-coms/server.js
+++ serial-coms/server.js
@@ -1,4 +1,13 @@
 'use strict';
+
+// const SerialPort = require('serialport');
+// const port = new SerialPort('/dev/ttyUSB1', {
+//   baudRate: 57600
+// });
+// const Readline = SerialPort.parsers.Readline;
+// const parser = new Readline();
+// port.pipe(parser);
+
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
@@ -7,11 +16,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/goto', (req, res) => {
   console.log(req.params);
-  console.log(req.params.position);
+  console.log(req.body.position);
   console.log(req.body);
   res.send('holi');
 
